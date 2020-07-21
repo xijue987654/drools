74d5870c650e560b2b2efc50deb1ae64015a9b41
mashuai <1967113456@qq.com>
2020-07-21 23:23:03 +0800

    update

---
+++ src/test/java/com/xijue/drools/Test.java
@@ -0,0 +1,15 @@
+package com.xijue.drools;
+
+/**
+ * ClassName: Test
+ * Description: test
+ * Author: xijue987654
+ * Date: 2020/7/21 23:22
+ * Version: 1.0
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.printf("");
+    }
+}
